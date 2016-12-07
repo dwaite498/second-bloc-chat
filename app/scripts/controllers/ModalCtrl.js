40e0fa4b066a3b0110c914b85082fed9040f500f
Davey <dwaite498@gmail.com>
2016-12-07 18:02:02 -0500

    early modal functionality

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,30 @@
+(function() {
+    function roomModal($scope, $uibModal, $log) {
+        var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
+
+        
+
+    
+    angular
+        .module('blocChat')
+        .controller('roomModal', ['ui.bootstrap', roomModal])
+})();
